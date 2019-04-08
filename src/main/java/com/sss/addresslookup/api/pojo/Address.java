6c9ccee9554c59c764c059e2da5bd39c940d56af
vinayv1979 <vinay.vijayakumar79@gmail.com>
2019-04-08 15:55:07 +0100

    Test123

--- src/main/java/com/sss/addresslookup/api/pojo/Address.java
+++ src/main/java/com/sss/addresslookup/api/pojo/Address.java
@@ -8,47 +8,32 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-
-
-@XmlRootElement (name="address")
+@XmlRootElement(name = "address")
 @XmlAccessorType(XmlAccessType.FIELD)
-public class Address implements Serializable{
-	
-	
-	
-	
-	
+public class Address implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	@XmlElement(name = "results")
 	private ArrayList<Results> results;
-	
-	public int getTotalResultsXXXX() {
-		return totalResultsXXXX;
+
+	@XmlElement(name = "totalResults")
+	private int totalResults;
+
+	public int getTotalResults() {
+		return totalResults;
 	}
 
-
-	public void setTotalResultsXXXX(int totalResultsXXXX) {
-		this.totalResultsXXXX = totalResultsXXXX;
+	public void setTotalResults(int totalResults) {
+		this.totalResults = totalResults;
 	}
-
-
-	@XmlElement(name = "totalResultsXXXX")
-	private int totalResultsXXXX; 
-
-
-
-
-	
 
 	public ArrayList<Results> getResults() {
 		return results;
 	}
-
 
 	public void setResults(ArrayList<Results> results) {
 		this.results = results;
